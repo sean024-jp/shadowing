f05221ce364fc7e34adbd31e4bd54e4b6f8204ff
sean024 <headachers024@0gmail.com>
2025-03-10 18:48:09 +0900

    Day 2: プロジェクト構造の理解とシンプルな画面の作成

---
+++ src/HelloScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HelloScreen() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h1>Welcome to My Shadowing App!</h1>
+      <p>Let's start building your app!</p>
+    </div>
+  );
+}
+
+export default HelloScreen;
